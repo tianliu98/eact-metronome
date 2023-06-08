83a6bf527a85956ef8c2bed5aa7a659843a490ea
tianliu98 <lt6363185@gmail.com>
2023-06-08 18:47:19 -0500

    modify styling

--- src/components/Metronome.jsx
+++ src/components/Metronome.jsx
@@ -1,90 +1,91 @@
 import React, { useEffect, useRef, useState, createRef } from "react";
-import "./Metronome.css";
-import tick from "../sound/tick-sound.wav";
+import "./Metronome.scss";
+import BeatBlock from "./BeatBlock/BeatBlock";
 
 const Metronome = () => {
-  const dotsContainerRef = useRef(null);
-  const [timer, setTimer] = useState(null);
-  const [bpm, setBpm] = useState(60);
-  const [beat, setBeat] = useState(4);
-  const tickSound = new Audio(tick);
-  const [dotRefs, setDotRefs] = useState([]);
-  const [activeIdx, setActiveIdx] = useState(-1);
+    const dotsContainerRef = useRef(null);
+    const [timer, setTimer] = useState(null);
+    const [bpm, setBpm] = useState(30);
+    const [beat, setBeat] = useState(4);
+    const [dotRefs, setDotRefs] = useState([]);
+    const [activeIdx, setActiveIdx] = useState(-1);
 
-  const dotLooper = (duration, dots) => {
-    const numDots = dots.length;
-    const gap = duration / dots.length;
-    for (let i = 0; i < numDots; i++) {
-      setTimeout(() => {
-        tickSound.play();
-        console.log(i);
-        setActiveIdx(i);
-      }, gap * i);
-    }
-    console.log(numDots, gap);
-  };
+    const dotLooper = (duration, dots) => {
+        const numDots = dots.length;
+        const gap = duration / dots.length;
+        setActiveIdx(0); // Set the active index to 0.
 
-  const onPlay = () => {
-    console.log(`Playing, bpm: ${bpm}`);
-    clearInterval(timer);
-    const newTimer = setInterval(() => {
-      dotLooper((60 / bpm) * 1000, dotsContainerRef.current.children);
-    }, (60 / bpm) * 1000);
-    setTimer(newTimer);
-  };
+        for (let i = 1; i < numDots; i++) {
+            // start from 1 since we've played the 0 index sound
+            setTimeout(() => {
+                setActiveIdx(i);
+            }, gap * i);
+        }
+    };
 
-  const onStop = () => {
-    clearInterval(timer);
-  };
+    const onPlay = () => {
+        clearInterval(timer);
+        dotLooper((60 / bpm) * 1000, dotsContainerRef.current.children);
+        const newTimer = setInterval(() => {
+            dotLooper((60 / bpm) * 1000, dotsContainerRef.current.children);
+        }, (60 / bpm) * 1000);
+        setTimer(newTimer);
+    };
 
-  const beatChangeHandler = (e) => {
-    setBeat(e.target.value);
-  };
+    const onStop = () => {
+        clearInterval(timer);
+    };
 
-  const subBeat = () => {
-    if (beat > 1) setBeat((prevBeat) => prevBeat - 1);
-  };
+    const beatChangeHandler = (e) => {
+        setBeat(e.target.value);
+    };
 
-  const addBeat = () => {
-    if (beat < 16) setBeat((prevBeat) => prevBeat + 1);
-  };
+    const subBeat = () => {
+        if (beat > 1) setBeat((prevBeat) => prevBeat - 1);
+    };
 
-  const onBpmChange = (e) => {
-    setBpm(e.target.value);
-  }
+    const addBeat = () => {
+        if (beat < 16) setBeat((prevBeat) => prevBeat + 1);
+    };
 
-  useEffect(() => {
-    setDotRefs((prev) => {
-      return Array(beat)
-        .fill()
-        .map((_, i) => prev[i] || createRef());
-    });
-  }, [beat]);
+    const onBpmChange = (e) => {
+        setBpm(e.target.value);
+    };
 
-  return (
-    <div>
-      <div>Hello</div>
-      <div className="dots" ref={dotsContainerRef}>
-        {Array.from({ length: beat }, (v, i) => i).map((v, i) => (
-          <div
-            className={`circle ${i === 0 ? "first-dot" : ""} ${
-              i === activeIdx ? "active" : ""
-            }`}
-            key={i}
-            ref={dotRefs[i]}
-          ></div>
-        ))}
-      </div>
-      <button onClick={onPlay}>Play</button>
-      <button onClick={onStop}>Stop</button>
-      <div className="setting">
-        <button onClick={subBeat}>-</button>
-        <input value={beat} onChange={beatChangeHandler} />
-        <button onClick={addBeat}>+</button>
-        <input type="text" value={bpm} onChange={onBpmChange}/>
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        setDotRefs((prev) => {
+            return Array(beat)
+                .fill()
+                .map((_, i) => prev[i] || createRef());
+        });
+    }, [beat]);
+
+    return (
+        <div className="metronome-container">
+            <div className="dots" ref={dotsContainerRef}>
+                {Array.from({ length: beat }, (v, i) => i).map((v, i) => (
+                    <BeatBlock
+                        key={i}
+                        ref={dotRefs[i]}
+                        isPlaying={i === activeIdx ? true : false}
+                    />
+                ))}
+            </div>
+            <button onClick={onPlay}>Play</button>
+            <button onClick={onStop}>Stop</button>
+            <div className="setting">
+                <div className="beats-setting">
+                    <button onClick={subBeat}>-</button>
+                    <input value={beat} onChange={beatChangeHandler} />
+                    <button onClick={addBeat}>+</button>
+                </div>
+
+                <div className="bpm-setting">
+                    <input type="text" value={bpm} onChange={onBpmChange} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Metronome;
